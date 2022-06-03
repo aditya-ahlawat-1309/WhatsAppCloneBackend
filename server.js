4c97accd3b27bad6c60151d7990fc096f988ba7a
aditya-ahlawat-1309 <adityaroyal10@gmail.com>
2022-06-03 15:35:07 +0530

    improved 6

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(function (req, res, next) {
     // Website you wish to allow to connect
     res.setHeader(
       "Access-Control-Allow-Origin",
-      "https://chatfullstack.netlify.app/"
+      "https://chatfullstack.netlify.app"
     );
 
     // Request methods you wish to allow
