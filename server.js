c7590fe6b94f42821383eec373271f7e22c5f24c
aditya-ahlawat-1309 <adityaroyal10@gmail.com>
2022-06-03 15:12:25 +0530

    improved version

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ const app = express();
 
 app.use(express.json()); // to accept json data
 
+app.use(function (req,res,next) {
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // app.get("/", (req, res) => {
 //   res.send("API Running!");
 // });
