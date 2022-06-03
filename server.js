f00866ab2679cea9e15817cb410eb5041c31baea
aditya-ahlawat-1309 <adityaroyal10@gmail.com>
2022-06-03 15:17:07 +0530

    improved 2

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ const app = express();
 app.use(express.json()); // to accept json data
 
 app.use(function (req,res,next) {
-  res.header("Access-Control-Allow-Origin","*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://chatfullstack.netlify.app/"
+  );
   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
