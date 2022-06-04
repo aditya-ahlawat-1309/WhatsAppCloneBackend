229f008f178d3ef075c3fbc095c45f0f1d464f01
aditya-ahlawat-1309 <adityaroyal10@gmail.com>
2022-06-04 08:20:22 +0530

    final 2

--- server.js
+++ server.js
@@ -109,8 +109,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://chatfullstack.netlify.app/",
-    // credentials: true,
+    origin: '*',
   },
 });
 
